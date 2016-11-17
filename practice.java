4a5e3db866cbc3e08ce25681b3a5f75908b613d4
Linse <linsemathew123@gmail.com>
2016-11-17 12:31:07 -0500

    Make bmi calculator.

--- practice.java
+++ practice.java
@@ -11,5 +11,21 @@ public class Practice {
    		// Scanner input = new Scanner(System.in);
    		// String userName = input.next();
    		// System.out.println("Your name is " + userName);
+
+   		// Scanner input = new Scanner(System.in);
+   		// System.out.println("What is your weight in pounds?");
+   		// double weight = input.nextDouble();
+   		// System.out.println("What is your height in inches?");
+   		// double height = input.nextDouble();
+
+   		// final double KILOGRAMS_PER_POUND = 0.45359237;
+   		// final double METERS_PER_INCH = 0.0254;
+
+   		// double weight_in_kilograms = weight * KILOGRAMS_PER_POUND;
+   		// double height_in_meters = height * METERS_PER_INCH;
+
+   		// double bmi = weight_in_kilograms / (height_in_meters * height_in_meters);
+
+   		// System.out.println("Your bmi is " + bmi);
    }
 }
