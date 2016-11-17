8735d8dd6b5c27500102ff0f4ebef38321670a56
Linse <linsemathew123@gmail.com>
2016-11-17 12:08:33 -0500

    Test Math.random()

--- practice.java
+++ practice.java
@@ -1,5 +1,8 @@
 public class Practice { 
    public static void main(String[] args) { 
-        System.out.println("Hello, World");
+        // System.out.println("Hello, World");
+
+   		// double number = Math.random();
+   		// System.out.println(number);
    }
 }
