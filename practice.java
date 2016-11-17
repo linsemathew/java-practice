a5f0a837153385c6b79478b7c58356f1645ce811
Linse <linsemathew123@gmail.com>
2016-11-17 12:17:18 -0500

    Test Scanner.

--- practice.java
+++ practice.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 public class Practice { 
    public static void main(String[] args) { 
         // System.out.println("Hello, World");
 
    		// double number = Math.random();
    		// System.out.println(number);
+
+   		// System.out.println("What is your name?");
+   		// Scanner input = new Scanner(System.in);
+   		// String userName = input.next();
+   		// System.out.println("Your name is " + userName);
    }
 }
